7f9962ba4a0754da57d8a06f6c2712acdd159a84
flyingbutton <thedrafttin@gmail.com>
2026-01-15 17:26:12 -0500

    Renamed src/ to client/ folder

---
+++ client/src/extension.ts
@@ -0,0 +1,55 @@
+import * as path from "path";
+import * as vscode from "vscode";
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind,
+} from "vscode-languageclient/node";
+
+let client: LanguageClient | undefined;
+
+// Start the client with server side attached
+export function activate(context: vscode.ExtensionContext): void {
+  const serverModule = context.asAbsolutePath(
+    path.join("server", "out", "server.js"),
+  );
+
+  const serverOptions: ServerOptions = {
+    run: { module: serverModule, transport: TransportKind.stdio },
+    debug: {
+      module: serverModule,
+      transport: TransportKind.stdio,
+      options: { execArgv: ["--nolazy", "--inspect=6009"] },
+    },
+  };
+
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: [{ scheme: "file", language: "umple" }],
+    initializationOptions: {
+      umpleSyncJarPath: context.asAbsolutePath("umplesync.jar"),
+      umpleSyncPort: 5556,
+    },
+    synchronize: {
+      fileEvents: vscode.workspace.createFileSystemWatcher("**/*.ump"),
+    },
+  };
+
+  client = new LanguageClient(
+    "umpleLanguageServer",
+    "Umple Language Server",
+    serverOptions,
+    clientOptions,
+  );
+
+  client.start();
+  // context.subscriptions.push();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+  if (!client) {
+    return undefined;
+  }
+
+  return client.stop();
+}
